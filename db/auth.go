f5c4e2d4d4156ee6d70529ad08cfb5837fbc7f45
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-04-21 20:04:31 +0200

    auth repo

--- db/auth.go
+++ db/auth.go
@@ -24,6 +24,55 @@ func NewAuthRepository(filename string) (*AuthRepository, error) {
 	return &AuthRepository{db: db_}, nil
 }
 
+type AuthEntry struct {
+	ID        *int64 `json:"id"`
+	Name      string `json:"name"`
+	Password  string `json:"password"`
+	LastLogin int    `json:"last_login"`
+}
+
+type PrivilegeEntry struct {
+	ID        int64  `json:"id"`
+	Privilege string `json:"privilege"`
+}
+
 type AuthRepository struct {
 	db *sql.DB
+}
+
+func (repo *AuthRepository) GetByUsername(username string) (*AuthEntry, error) {
+	rows, err := repo.db.Query("select id,name,password,last_login from auth where name = $1", username)
+	if err != nil {
+		return nil, err
+	}
+	if !rows.Next() {
+		return nil, nil
+	}
+	entry := &AuthEntry{}
+	err = rows.Scan(&entry.ID, &entry.Name, &entry.Password, &entry.LastLogin)
+	return entry, err
+}
+
+func (repo *AuthRepository) Create(entry *AuthEntry) error {
+	result, err := repo.db.Exec("insert into auth(id,name,password,last_login) values($1,$2,$3,$4)", entry.ID, entry.Name, entry.Password, entry.LastLogin)
+	if err != nil {
+		return err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return err
+	}
+	// assign returned id
+	entry.ID = &id
+	return nil
+}
+
+func (repo *AuthRepository) Update(entry *AuthEntry) error {
+	_, err := repo.db.Exec("update auth set name = $1, password = $2, last_login = $3 where id = $4", entry.Name, entry.Password, entry.LastLogin, entry.ID)
+	return err
+}
+
+func (repo *AuthRepository) Delete(id int64) error {
+	_, err := repo.db.Exec("delete from auth where id = $1", id)
+	return err
 }
---
+++ db/auth_test.go
@@ -0,0 +1,63 @@
+package db
+
+import (
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAuthRepo(t *testing.T) {
+	// init stuff
+	dbfile, err := os.CreateTemp(os.TempDir(), "auth.sqlite")
+	assert.NoError(t, err)
+	assert.NotNil(t, dbfile)
+
+	// open db
+	repo, err := NewAuthRepository(dbfile.Name())
+	assert.NoError(t, err)
+	assert.NotNil(t, repo)
+
+	// non-existing entry
+	entry, err := repo.GetByUsername("bogus")
+	assert.NoError(t, err)
+	assert.Nil(t, entry)
+
+	// create entry
+	new_entry := &AuthEntry{
+		Name:      "createduser",
+		Password:  "blah",
+		LastLogin: 456,
+	}
+	assert.NoError(t, repo.Create(new_entry))
+	assert.NotNil(t, new_entry.ID)
+
+	// check newly created entry
+	entry, err = repo.GetByUsername("createduser")
+	assert.NoError(t, err)
+	assert.NotNil(t, entry)
+	assert.Equal(t, new_entry.Name, entry.Name)
+	assert.Equal(t, new_entry.Password, entry.Password)
+	assert.Equal(t, *new_entry.ID, *entry.ID)
+	assert.Equal(t, new_entry.LastLogin, entry.LastLogin)
+
+	// change things
+	new_entry.Name = "x"
+	new_entry.Password = "y"
+	new_entry.LastLogin = 123
+	assert.NoError(t, repo.Update(new_entry))
+	entry, err = repo.GetByUsername("x")
+	assert.NoError(t, err)
+	assert.NotNil(t, entry)
+	assert.Equal(t, new_entry.Name, entry.Name)
+	assert.Equal(t, new_entry.Password, entry.Password)
+	assert.Equal(t, *new_entry.ID, *entry.ID)
+	assert.Equal(t, new_entry.LastLogin, entry.LastLogin)
+
+	// remove new user
+	assert.NoError(t, repo.Delete(*new_entry.ID))
+	entry, err = repo.GetByUsername("x")
+	assert.NoError(t, err)
+	assert.Nil(t, entry)
+
+}
