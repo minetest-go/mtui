f421fd827a694cf3b49546ddf3b9958fb6f0ed86
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-04-21 11:25:30 +0200

    missing if not exists

--- main.go
+++ main.go
@@ -16,15 +16,15 @@ CREATE TABLE if not exists user_privileges (id INTEGER,privilege VARCHAR(32),PRI
 
 const PLAYERS_MIGRATE = `
 pragma journal_mode = wal;
-CREATE TABLE player (name VARCHAR(50) NOT NULL,pitch NUMERIC(11, 4) NOT NULL,yaw NUMERIC(11, 4) NOT NULL,posX NUMERIC(11, 4) NOT NULL,posY NUMERIC(11, 4) NOT NULL,posZ NUMERIC(11, 4) NOT NULL,hp INT NOT NULL,breath INT NOT NULL,creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,modification_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (name));
-CREATE TABLE player_metadata (player VARCHAR(50) NOT NULL,metadata VARCHAR(256) NOT NULL,value TEXT,PRIMARY KEY(player, metadata),FOREIGN KEY (player) REFERENCES player (name) ON DELETE CASCADE );
-CREATE TABLE player_inventories (layer VARCHAR(50) NOT NULL,inv_id INT NOT NULL,inv_width INT NOT NULL,inv_name TEXT NOT NULL DEFAULT '',inv_size INT NOT NULL,PRIMARY KEY(player, inv_id),   FOREIGN KEY (player) REFERENCES player (name) ON DELETE CASCADE );
-CREATE TABLE player_inventory_items (   player VARCHAR(50) NOT NULL,inv_id INT NOT NULL,slot_id INT NOT NULL,item TEXT NOT NULL DEFAULT '',PRIMARY KEY(player, inv_id, slot_id),   FOREIGN KEY (player) REFERENCES player (name) ON DELETE CASCADE );
+CREATE TABLE if not exists player (name VARCHAR(50) NOT NULL,pitch NUMERIC(11, 4) NOT NULL,yaw NUMERIC(11, 4) NOT NULL,posX NUMERIC(11, 4) NOT NULL,posY NUMERIC(11, 4) NOT NULL,posZ NUMERIC(11, 4) NOT NULL,hp INT NOT NULL,breath INT NOT NULL,creation_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,modification_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,PRIMARY KEY (name));
+CREATE TABLE if not exists player_metadata (player VARCHAR(50) NOT NULL,metadata VARCHAR(256) NOT NULL,value TEXT,PRIMARY KEY(player, metadata),FOREIGN KEY (player) REFERENCES player (name) ON DELETE CASCADE );
+CREATE TABLE if not exists player_inventories (layer VARCHAR(50) NOT NULL,inv_id INT NOT NULL,inv_width INT NOT NULL,inv_name TEXT NOT NULL DEFAULT '',inv_size INT NOT NULL,PRIMARY KEY(player, inv_id),   FOREIGN KEY (player) REFERENCES player (name) ON DELETE CASCADE );
+CREATE TABLE if not exists player_inventory_items (   player VARCHAR(50) NOT NULL,inv_id INT NOT NULL,slot_id INT NOT NULL,item TEXT NOT NULL DEFAULT '',PRIMARY KEY(player, inv_id, slot_id),   FOREIGN KEY (player) REFERENCES player (name) ON DELETE CASCADE );
 `
 
 const MOD_STORAGE_MIGRATE = `
 pragma journal_mode = wal;
-CREATE TABLE entries (
+CREATE TABLE if not exists entries (
 	modname TEXT NOT NULL,
 	key BLOB NOT NULL,
 	value BLOB NOT NULL,
@@ -34,7 +34,7 @@ CREATE TABLE entries (
 
 const MAP_MIGRATE = `
 pragma journal_mode = wal;
-CREATE TABLE blocks (
+CREATE TABLE if not exists blocks (
 	pos INT PRIMARY KEY,
 	data BLOB
 );
