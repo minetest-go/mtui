c1a2f65fcdb277c10874b3262116b9f759afc46e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-05-31 21:16:38 +0200

    SecurePriv() function

--- web/checks.go
+++ web/checks.go
@@ -25,9 +25,32 @@ func Secure(fn SecureHandlerFunc) http.HandlerFunc {
 		if err == err_unauthorized {
 			SendError(w, 401, "unauthorized")
 			return
-		}
-		if err != nil {
+		} else if err != nil {
 			SendError(w, 500, err.Error())
+			return
+		}
+		fn(w, r, claims)
+	}
+}
+
+func SecurePriv(required_priv string, fn SecureHandlerFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		claims, err := GetClaims(r)
+		if err == err_unauthorized {
+			SendError(w, 401, "unauthorized")
+			return
+		} else if err != nil {
+			SendError(w, 500, err.Error())
+			return
+		}
+		has_priv := false
+		for _, priv := range claims.Privileges {
+			if priv == required_priv {
+				has_priv = true
+			}
+		}
+		if !has_priv {
+			SendError(w, 403, "forbidden, missing priv: "+required_priv)
 			return
 		}
 		fn(w, r, claims)
