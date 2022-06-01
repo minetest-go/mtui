6e79306a6a8b7a4d738b5e94072dd893706d834b
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-06-01 16:18:54 +0200

    switch to plain boostrap

--- public/embed.go
+++ public/embed.go
@@ -5,7 +5,7 @@ import (
 )
 
 //go:embed js/* index.html
-//go:embed node_modules/bootswatch/dist/cyborg/bootstrap.min.css
+//go:embed node_modules/bootstrap/dist/css/bootstrap.min.css
 //go:embed node_modules/vue/dist/vue.global.prod.js
 //go:embed node_modules/vue-router/dist/vue-router.global.prod.js
 //go:embed node_modules/vue-i18n/dist/vue-i18n.global.prod.js
