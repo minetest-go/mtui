ddecf4123a6957f29522cbe5edab26d2d243291a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-06-01 16:24:54 +0200

    profile page

--- public/js/components/pages/Profile.js
+++ public/js/components/pages/Profile.js
@@ -21,13 +21,46 @@ export default {
                     {{ claims.username }}
                 </small>
             </h3>
-            <hr>
-            <h5>Privileges</h5>
-            <ul v-for="priv in claims.privileges">
-                <li>
-                    <span v-bind:class="getPrivBadgeClass(priv)">{{ priv }}</span>
-                </li>
-            </ul>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                            Privileges
+                        </div>
+                        <div class="card-body">
+                            <ul v-for="priv in claims.privileges">
+                                <li>
+                                    <span v-bind:class="getPrivBadgeClass(priv)">{{ priv }}</span>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                            Login stats
+                        </div>
+                        <div class="card-body">
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <div class="card-header">
+                            Password
+                        </div>
+                        <div class="card-body">
+                            <input type="password" class="form-control" placeholder="Old password"/>
+                            <input type="password" class="form-control" placeholder="New password"/>
+                            <input type="password" class="form-control" placeholder="New password (again)"/>
+                            <a class="btn btn-primary w-100">
+                                Change password
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     `
 };
