c5f21e2128e8dae4f9dc881100db59e957d3b5c9
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-06-08 18:23:48 +0200

    source link

--- public/js/components/pages/Start.js
+++ public/js/components/pages/Start.js
@@ -5,6 +5,17 @@ export default {
 		<div class="text-center">
 			<h4>Start page</h4>
 			<hr/>
+			<router-link to="/shell" class="btn btn-primary">
+				<i class="fa-solid fa-terminal"></i> Shell
+			</router-link>
+			&nbsp;
+			<router-link to="/profile" class="btn btn-primary">
+				<i class="fa fa-user"></i> Profile
+			</router-link>
+			&nbsp;
+			<a class="btn btn-secondary" href="https://github.com/minetest-go/mtui" target="new">
+				<i class="fa-brands fa-github"></i> Source
+			</a>
 		</div>
 	</div>
 	`
