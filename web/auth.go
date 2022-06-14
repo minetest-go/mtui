5b7bdd91d0254aa00c248e384e6203c8a5b28c67
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-06-14 15:52:49 +0200

    typo / blank pw field

--- web/auth.go
+++ web/auth.go
@@ -13,10 +13,14 @@ func (a *Api) GetAuth(w http.ResponseWriter, r *http.Request, claims *types.Clai
 
 	if playername != claims.Username && !claims.HasPriv("ban") {
 		// only players with the "ban" priv can look up other players auth
-		SendError(w, 403, "unauthoized")
+		SendError(w, 403, "unauthorized")
 		return
 	}
 
 	auth, err := a.app.DBContext.Auth.GetByUsername(playername)
+	if auth != nil {
+		// blank out password field
+		auth.Password = ""
+	}
 	Send(w, auth, err)
 }
