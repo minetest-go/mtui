091bf7ceabe3536c7e51a51438d1a8cdab617055
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-07-19 11:47:43 +0200

    fix empty mail list error

--- public/js/service/mail.js
+++ public/js/service/mail.js
@@ -1,5 +1,7 @@
 import { list, contacts } from "../api/mail.js";
 import store from "../store/mail.js";
 
-export const fetch_mails = () => list().then(l => store.mails = l.sort((a,b) => a.time < b.time));
+export const fetch_mails = () => list()
+    .then(l => l || [])
+    .then(l => store.mails = l.sort((a,b) => a.time < b.time));
 export const fetch_contacts = () => contacts().then(c => store.contacts = c);
