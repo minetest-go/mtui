2d199226397150ff2025344bd9696717a4db568d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-07-19 11:33:16 +0200

    nicer shell ui and feedback

--- public/js/components/pages/Shell.js
+++ public/js/components/pages/Shell.js
@@ -8,7 +8,8 @@ const store = Vue.reactive({
     success: false,
     error: false,
     busy: false,
-    message: ""
+    message: "",
+    delay: 0
 });
 
 export default {
@@ -24,6 +25,8 @@ export default {
             this.busy = true;
             this.error = false;
             this.success = false;
+            this.delay = 0;
+            const start = Date.now();
 
             execute_chatcommand(this.login_store.claims.username, this.command)
             .then(result => {
@@ -31,6 +34,7 @@ export default {
                 this.success = result.success;
                 this.error = !this.success;
                 this.message = result.message;
+                this.delay = Date.now() - start;
             });
         }
     },
@@ -41,21 +45,26 @@ export default {
                 <input type="text" placeholder="Command" v-model="command" class="form-control"/>
             </div>
             <div class="col-md-2">
-                <button class="btn btn-primary" type="submit" class="form-control" :disabled="!command">
+                <button class="btn btn-outline-primary" type="submit" class="form-control" :disabled="!command">
                     Execute
+                    &nbsp;
                     <i class="fa-solid fa-check" v-if="success" style="color: green;"></i>
                     <i class="fa-solid fa-xmark" v-if="error" style="color: red;"></i>
                     <i class="fa-solid fa-spinner fa-spin" v-if="busy"></i>
+                    &nbsp;
+                    <span v-if="delay">
+                        ({{delay}} ms)
+                    </span>
                 </button>
             </div>
         </form>
         <div class="row">
             <div class="col-md-12">
-            <a class="btn btn-secondary" v-on:click="predefined('status')" v-if="has_priv('interact')">status</a>
-            <a class="btn btn-secondary" v-on:click="predefined('privs')" v-if="has_priv('interact')">privs</a>
-            <a class="btn btn-secondary" v-on:click="predefined('days')" v-if="has_priv('interact')">days</a>
-            <a class="btn btn-secondary" v-on:click="predefined('time 6000')" v-if="has_priv('settime')">time 6000</a>
-            <a class="btn btn-secondary" v-on:click="predefined('shutdown 120 -r')" v-if="has_priv('server')">shutdown 120 -r</a>
+            <a class="btn btn-outline-secondary" v-on:click="predefined('status')" v-if="has_priv('interact')">status</a>
+            <a class="btn btn-outline-secondary" v-on:click="predefined('privs')" v-if="has_priv('interact')">privs</a>
+            <a class="btn btn-outline-secondary" v-on:click="predefined('days')" v-if="has_priv('interact')">days</a>
+            <a class="btn btn-outline-secondary" v-on:click="predefined('time 6000')" v-if="has_priv('settime')">time 6000</a>
+            <a class="btn btn-outline-secondary" v-on:click="predefined('shutdown 120 -r')" v-if="has_priv('server')">shutdown 120 -r</a>
             </div>
         </div>
         <hr>
