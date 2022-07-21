19afd4ece22de26b3d60f9944d23353980429e72
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-07-21 17:40:29 +0200

    owned areas endpoint

--- web/areas.go
+++ web/areas.go
@@ -5,9 +5,24 @@ import (
 	"mtui/types"
 	"net/http"
 	"path"
+
+	"github.com/gorilla/mux"
 )
 
 func (a *Api) GetAreas(w http.ResponseWriter, r *http.Request, claims *types.Claims) {
 	areas, err := areasparser.ParseFile(path.Join(a.app.WorldDir, "areas.json"))
 	Send(w, areas, err)
+}
+
+func (a *Api) GetOwnedAreas(w http.ResponseWriter, r *http.Request, claims *types.Claims) {
+	vars := mux.Vars(r)
+	playername := vars["playername"]
+	areas, err := areasparser.ParseFile(path.Join(a.app.WorldDir, "areas.json"))
+	owned_areas := make([]*areasparser.Area, 0)
+	for _, a := range areas {
+		if a.Owner == playername {
+			owned_areas = append(owned_areas, a)
+		}
+	}
+	Send(w, owned_areas, err)
 }
--- web/setup.go
+++ web/setup.go
@@ -23,21 +23,30 @@ func Setup(a *app.App) error {
 	}
 
 	r.HandleFunc("/api/ws", api.Websocket)
+
+	r.HandleFunc("/api/features", api.GetFeatures).Methods(http.MethodGet)
+
 	r.HandleFunc("/api/login", api.DoLogout).Methods(http.MethodDelete)
 	r.HandleFunc("/api/login", api.DoLogin).Methods(http.MethodPost)
 	r.HandleFunc("/api/login", api.GetLogin).Methods(http.MethodGet)
-	r.HandleFunc("/api/features", api.GetFeatures).Methods(http.MethodGet)
+
+	r.HandleFunc("/api/changepw", Secure(api.ChangePassword)).Methods(http.MethodPost)
+
+	r.HandleFunc("/api/auth/{playername}", Secure(api.GetAuth)).Methods(http.MethodGet)
+
 	r.HandleFunc("/api/areas", Secure(api.GetAreas)).Methods(http.MethodGet)
+	r.HandleFunc("/api/areas/{playername}", Secure(api.GetOwnedAreas)).Methods(http.MethodGet)
+
 	r.HandleFunc("/api/skin", Secure(api.GetSkin)).Methods(http.MethodGet)
 	r.HandleFunc("/api/skin", Secure(api.SetSkin)).Methods(http.MethodPost)
-	r.HandleFunc("/api/auth/{playername}", Secure(api.GetAuth)).Methods(http.MethodGet)
+
 	r.HandleFunc("/api/mail/list", Secure(api.GetMails)).Methods(http.MethodGet)
 	r.HandleFunc("/api/mail/{sender}/{time}", Secure(api.DeleteMail)).Methods(http.MethodDelete)
 	r.HandleFunc("/api/mail/{sender}/{time}/read", Secure(api.MarkRead)).Methods(http.MethodPost)
 	r.HandleFunc("/api/mail/checkrecipient/{recipient}", Secure(api.CheckValidRecipient)).Methods(http.MethodGet)
 	r.HandleFunc("/api/mail/send/{recipient}", Secure(api.SendMail)).Methods(http.MethodPost)
 	r.HandleFunc("/api/mail/contacts", Secure(api.GetContacts)).Methods(http.MethodGet)
-	r.HandleFunc("/api/changepw", Secure(api.ChangePassword)).Methods(http.MethodPost)
+
 	r.HandleFunc("/api/bridge/execute_chatcommand", Secure(api.ExecuteChatcommand)).Methods(http.MethodPost)
 	r.HandleFunc("/api/bridge", CheckApiKey(os.Getenv("APIKEY"), a.Bridge.HandlePost)).Methods(http.MethodPost)
 	r.HandleFunc("/api/bridge", CheckApiKey(os.Getenv("APIKEY"), a.Bridge.HandleGet)).Methods(http.MethodGet)
