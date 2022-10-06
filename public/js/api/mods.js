b16b6b305246f535b81b59a23c9f7bad6335eab7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-10-06 08:06:42 +0200

    partially working mod add/remove

--- public/js/api/mods.js
+++ public/js/api/mods.js
@@ -1,3 +1,10 @@
 export const list = () => fetch("api/mods").then(r => r.json());
 
 export const scan = () => fetch("api/mods/scan", {method: "POST"}).then(r => r.json());
+
+export const create = mod => fetch("api/mods", {
+    method: "POST",
+    body: JSON.stringify(mod)
+}).then(r => r.json());
+
+export const remove = id => fetch(`api/mods/${id}`, {method: "DELETE"});
--- public/js/components/pages/Mods.js
+++ public/js/components/pages/Mods.js
@@ -1,7 +1,14 @@
-import { list, scan } from '../../api/mods.js';
+import { list, scan, create, remove } from '../../api/mods.js';
 
 const store = Vue.reactive({
-	list: []
+	list: [],
+	scan_busy: false,
+	add_busy: false,
+	add_name: "",
+	add_mod_type: "mod",
+	add_source_type: "git",
+	add_url: "",
+	add_version: ""
 });
 
 export default {
@@ -13,18 +20,48 @@ export default {
 	},
 	methods: {
 		scan: function() {
+			this.scan_busy = true;
 			scan()
-			.then(() => this.update());
+			.then(() => {
+				this.update();
+				this.scan_busy = false;
+			});
+		},
+		add: function() {
+			this.add_busy = true;
+			create({
+				name: this.add_name,
+				mod_type: this.add_mod_type,
+				source_type: this.add_source_type,
+				url: this.add_url,
+				branch: "",
+				version: this.add_version
+			})
+			.then(() => {
+				this.add_busy = false;
+				this.add_name = "";
+				this.add_url = "";
+				this.add_version = "";
+				this.update();
+			});
 		},
 		update: function() {
 			list()
 			.then(l => this.list = l);
+		},
+		remove: function(id) {
+			remove(id)
+			.then(() => this.update());
 		}
 	},
 	template: /*html*/`
 		<div>
 			<h3>Mod selection</h3>
-			<table class="table table-condensed">
+			<button class="btn btn-primary" v-on:click="scan" :disabled="scan_busy">
+				<i class="fa fa-refresh"/>
+				Scan
+			</button>
+			<table class="table table-condensed table-striped">
 				<thead>
 					<tr>
 						<th>Type</th>
@@ -67,9 +104,11 @@ export default {
 							<div class="btn-group">
 								<a class="btn btn-primary">
 									<i class="fa fa-edit"></i>
+									Edit
 								</a>
-								<a class="btn btn-danger">
+								<a class="btn btn-danger" v-on:click="remove(mod.id)">
 									<i class="fa fa-times"></i>
+									Remove
 								</a>
 							</div>
 						</td>
@@ -78,31 +117,35 @@ export default {
 				<tbody>
 					<tr>
 						<td>
-							<select class="form-control">
-								<option>Mod</option>
-								<option>Game</option>
-								<option>Textures</option>
-							</select>
-						</td>
-						<td></td>
-						<td>
-							<select class="form-control">
-								<option>Git</option>
-								<option>ContentDB</option>
+							<select class="form-control" v-model="add_mod_type">
+								<option value="mod">Mod</option>
+								<option value="worldmods">World-Mods</option>
+								<option value="game">Game</option>
+								<option value="txp">Textures</option>
 							</select>
 						</td>
 						<td>
-							<input class="form-control" type="text" placeholder="Source url"/>
+							<input class="form-control" type="text" placeholder="Mod name" v-model="add_name"/>
 						</td>
 						<td>
-							<input class="form-control" type="text" placeholder="Version"/>
+							<select class="form-control" v-model="add_source_type">
+								<option value="git">Git</option>
+								<option value="cdb">ContentDB</option>
+							</select>
+						</td>
+						<td>
+							<input class="form-control" type="text" placeholder="Source url" v-model="add_url"/>
+						</td>
+						<td>
+							<input class="form-control" type="text" placeholder="Version" v-model="add_version"/>
 						</td>
 						<td>
 						</td>
 						<td>
-							<a class="btn btn-success">
-								<i class="fa fa-save"></i>
-							</a>
+							<button class="btn btn-success" v-on:click="add" :disabled="add_busy">
+								<i class="fa fa-plus"></i>
+								Add
+							</button>
 						</td>
 					</tr>
 				</tbody>
