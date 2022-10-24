07a1273180bf9600ac71a7a0805d2a2671b3627c
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2022-10-24 12:06:05 +0200

    add log stuff

---
+++ db/log_test.go
@@ -0,0 +1,36 @@
+package db_test
+
+import (
+	"mtui/db"
+	"mtui/types"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestLogRepo(t *testing.T) {
+	_db := setupDB(t)
+	repo := db.LogRepository{DB: _db}
+
+	l := &types.Log{}
+	assert.NoError(t, repo.Insert(l))
+	assert.NotEqual(t, "", l.ID)
+	assert.True(t, l.Timestamp > 0)
+
+	from_t := l.Timestamp - 2000
+	to_t := l.Timestamp + 2000
+	s := &types.LogSearch{
+		FromTimestamp: &from_t,
+		ToTimestamp:   &to_t,
+	}
+
+	count, err := repo.Count(s)
+	assert.NoError(t, err)
+	assert.Equal(t, int64(1), count)
+
+	list, err := repo.Search(s)
+	assert.NoError(t, err)
+	assert.NotNil(t, list)
+	assert.Equal(t, 1, len(list))
+	assert.Equal(t, l.ID, list[0].ID)
+}
