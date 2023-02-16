f2e77f6a01d4c6396c8274b421945f1462ed74c6
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-02-16 11:18:05 +0100

    http error

--- web/send.go
+++ web/send.go
@@ -4,9 +4,15 @@ import (
 	"bytes"
 	"encoding/json"
 	"net/http"
+
+	"github.com/sirupsen/logrus"
 )
 
 func SendError(w http.ResponseWriter, code int, message string) {
+	logrus.WithFields(logrus.Fields{
+		"code":    code,
+		"message": message,
+	}).Error("http error")
 	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
 	w.WriteHeader(code)
 	w.Write([]byte(message))
