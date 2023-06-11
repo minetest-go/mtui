210d0adf36bf4dc4f06896eaea7c837a731220d8
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-06-11 20:49:00 +0200

    more subrouter refactoring

--- types/privs.go
+++ types/privs.go
@@ -1,12 +1,9 @@
 package types
 
 const (
-	PRIV_SERVER     = "server"
-	PRIV_BAN        = "ban"
-	PRIV_AREAS      = "areas"
-	PRIV_INTERACT   = "interact"
-	PRIV_SKINSDB    = "skinsdb"
-	PRIV_MONITORING = "monitoring"
-	PRIV_MAIL       = "mail"
-	PRIV_XBAN       = "xban"
+	PRIV_SERVER   = "server"
+	PRIV_BAN      = "ban"
+	PRIV_AREAS    = "areas"
+	PRIV_INTERACT = "interact"
+	PRIV_XBAN     = "xban"
 )
--- web/setup.go
+++ web/setup.go
@@ -26,57 +26,61 @@ func Setup(a *app.App) error {
 		return err
 	}
 
-	r.HandleFunc("/api/version", api.GetVersion)
+	apir := r.PathPrefix("/api").Subrouter()
 
-	r.HandleFunc("/api/features", api.GetFeatures).Methods(http.MethodGet)
-	r.HandleFunc("/api/feature", api.SecurePriv(types.PRIV_SERVER, api.SetFeature)).Methods(http.MethodPost)
+	apir.HandleFunc("/version", api.GetVersion)
 
-	r.HandleFunc("/api/login", api.DoLogout).Methods(http.MethodDelete)
-	r.HandleFunc("/api/login", api.DoLogin).Methods(http.MethodPost)
-	r.HandleFunc("/api/login", api.GetLogin).Methods(http.MethodGet)
+	apir.HandleFunc("/features", api.GetFeatures).Methods(http.MethodGet)
+	apir.HandleFunc("/feature", api.SecurePriv(types.PRIV_SERVER, api.SetFeature)).Methods(http.MethodPost)
 
-	r.HandleFunc("/api/onboard", api.GetOnboardStatus).Methods(http.MethodGet)
-	r.HandleFunc("/api/onboard", api.CreateOnboardUser).Methods(http.MethodPost)
+	apir.HandleFunc("/login", api.DoLogout).Methods(http.MethodDelete)
+	apir.HandleFunc("/login", api.DoLogin).Methods(http.MethodPost)
+	apir.HandleFunc("/login", api.GetLogin).Methods(http.MethodGet)
 
-	r.HandleFunc("/api/geoip/{ip}", api.SecurePriv(types.PRIV_BAN, api.QueryGeoip)).Methods(http.MethodGet)
+	apir.HandleFunc("/onboard", api.GetOnboardStatus).Methods(http.MethodGet)
+	apir.HandleFunc("/onboard", api.CreateOnboardUser).Methods(http.MethodPost)
 
-	r.HandleFunc("/api/changepw", api.Secure(api.ChangePassword)).Methods(http.MethodPost)
+	apir.HandleFunc("/geoip/{ip}", api.SecurePriv(types.PRIV_BAN, api.QueryGeoip)).Methods(http.MethodGet)
 
-	r.HandleFunc("/api/oauth_app", api.SecurePriv(types.PRIV_SERVER, api.GetOauthApps)).Methods(http.MethodGet)
-	r.HandleFunc("/api/oauth_app", api.SecurePriv(types.PRIV_SERVER, api.SetOauthApp)).Methods(http.MethodPost)
-	r.HandleFunc("/api/oauth_app/{id}", api.SecurePriv(types.PRIV_SERVER, api.GetOauthAppByID)).Methods(http.MethodGet)
-	r.HandleFunc("/api/oauth_app/{id}", api.SecurePriv(types.PRIV_SERVER, api.DeleteOauthApp)).Methods(http.MethodDelete)
+	apir.HandleFunc("/changepw", api.Secure(api.ChangePassword)).Methods(http.MethodPost)
 
-	r.HandleFunc("/api/player/info/{playername}", api.SecurePriv(types.PRIV_INTERACT, api.GetPlayerInfo)).Methods(http.MethodGet)
-	r.HandleFunc("/api/player/search", api.SecurePriv(types.PRIV_INTERACT, api.SearchPlayer)).Methods(http.MethodPost)
-	r.HandleFunc("/api/player/count", api.SecurePriv(types.PRIV_INTERACT, api.CountPlayer)).Methods(http.MethodPost)
+	apir.HandleFunc("/oauth_app", api.SecurePriv(types.PRIV_SERVER, api.GetOauthApps)).Methods(http.MethodGet)
+	apir.HandleFunc("/oauth_app", api.SecurePriv(types.PRIV_SERVER, api.SetOauthApp)).Methods(http.MethodPost)
+	apir.HandleFunc("/oauth_app/{id}", api.SecurePriv(types.PRIV_SERVER, api.GetOauthAppByID)).Methods(http.MethodGet)
+	apir.HandleFunc("/oauth_app/{id}", api.SecurePriv(types.PRIV_SERVER, api.DeleteOauthApp)).Methods(http.MethodDelete)
 
-	r.HandleFunc("/api/log/search", api.SecurePriv(types.PRIV_BAN, api.SearchLogs)).Methods(http.MethodPost)
-	r.HandleFunc("/api/log/count", api.SecurePriv(types.PRIV_BAN, api.CountLogs)).Methods(http.MethodPost)
-	r.HandleFunc("/api/log/events/{category}", api.SecurePriv(types.PRIV_BAN, api.GetLogEvents)).Methods(http.MethodGet)
+	apir.HandleFunc("/player/info/{playername}", api.SecurePriv(types.PRIV_INTERACT, api.GetPlayerInfo)).Methods(http.MethodGet)
+	apir.HandleFunc("/player/search", api.SecurePriv(types.PRIV_INTERACT, api.SearchPlayer)).Methods(http.MethodPost)
+	apir.HandleFunc("/player/count", api.SecurePriv(types.PRIV_INTERACT, api.CountPlayer)).Methods(http.MethodPost)
 
-	r.HandleFunc("/api/areas", api.Feature(types.PRIV_AREAS, api.Secure(api.GetAreas))).Methods(http.MethodGet)
-	r.HandleFunc("/api/areas/{playername}", api.Feature(types.PRIV_AREAS, api.Secure(api.GetOwnedAreas))).Methods(http.MethodGet)
+	apir.HandleFunc("/log/search", api.SecurePriv(types.PRIV_BAN, api.SearchLogs)).Methods(http.MethodPost)
+	apir.HandleFunc("/log/count", api.SecurePriv(types.PRIV_BAN, api.CountLogs)).Methods(http.MethodPost)
+	apir.HandleFunc("/log/events/{category}", api.SecurePriv(types.PRIV_BAN, api.GetLogEvents)).Methods(http.MethodGet)
 
-	r.HandleFunc("/api/skin", api.Feature(types.PRIV_SKINSDB, api.Secure(api.GetSkin))).Methods(http.MethodGet)
-	r.HandleFunc("/api/skin", api.Feature(types.PRIV_SKINSDB, api.Secure(api.SetSkin))).Methods(http.MethodPost)
+	apir.HandleFunc("/areas", api.Feature(types.PRIV_AREAS, api.Secure(api.GetAreas))).Methods(http.MethodGet)
+	apir.HandleFunc("/areas/{playername}", api.Feature(types.PRIV_AREAS, api.Secure(api.GetOwnedAreas))).Methods(http.MethodGet)
 
-	r.HandleFunc("/api/metric_types", api.Feature(types.PRIV_MONITORING, api.GetMetricTypes)).Methods(http.MethodGet)
-	r.HandleFunc("/api/metric_types/{name}", api.Feature(types.PRIV_MONITORING, api.GetMetricType)).Methods(http.MethodGet)
-	r.HandleFunc("/api/metrics/search", api.Feature(types.PRIV_MONITORING, api.SearchMetrics)).Methods(http.MethodPost)
-	r.HandleFunc("/api/metrics/count", api.Feature(types.PRIV_MONITORING, api.CountMetrics)).Methods(http.MethodPost)
+	apir.HandleFunc("/skin", api.Feature(types.FEATURE_SKINSDB, api.Secure(api.GetSkin))).Methods(http.MethodGet)
+	apir.HandleFunc("/skin", api.Feature(types.FEATURE_SKINSDB, api.Secure(api.SetSkin))).Methods(http.MethodPost)
 
-	r.HandleFunc("/api/mail/folder/inbox", api.Feature(types.PRIV_MAIL, api.Secure(api.GetInbox))).Methods(http.MethodGet)
-	r.HandleFunc("/api/mail/folder/outbox", api.Feature(types.PRIV_MAIL, api.Secure(api.GetOutbox))).Methods(http.MethodGet)
-	r.HandleFunc("/api/mail/folder/drafts", api.Feature(types.PRIV_MAIL, api.Secure(api.GetDrafts))).Methods(http.MethodGet)
-	r.HandleFunc("/api/mail/contacts", api.Feature(types.PRIV_MAIL, api.Secure(api.GetContacts))).Methods(http.MethodGet)
-	r.HandleFunc("/api/mail", api.Feature(types.PRIV_MAIL, api.Secure(api.SendMail))).Methods(http.MethodPost)
-	r.HandleFunc("/api/mail/{id}", api.Feature(types.PRIV_MAIL, api.Secure(api.DeleteMail))).Methods(http.MethodDelete)
-	r.HandleFunc("/api/mail/{id}/read", api.Feature(types.PRIV_MAIL, api.Secure(api.MarkRead))).Methods(http.MethodPost)
-	r.HandleFunc("/api/mail/{id}/unread", api.Feature(types.PRIV_MAIL, api.Secure(api.MarkUnread))).Methods(http.MethodPost)
-	r.HandleFunc("/api/mail/checkrecipient/{recipient}", api.Feature(types.PRIV_MAIL, api.Secure(api.CheckValidRecipient))).Methods(http.MethodGet)
+	apir.HandleFunc("/metric_types", api.Feature(types.FEATURE_MONITORING, api.GetMetricTypes)).Methods(http.MethodGet)
+	apir.HandleFunc("/metric_types/{name}", api.Feature(types.FEATURE_MONITORING, api.GetMetricType)).Methods(http.MethodGet)
+	apir.HandleFunc("/metrics/search", api.Feature(types.FEATURE_MONITORING, api.SearchMetrics)).Methods(http.MethodPost)
+	apir.HandleFunc("/metrics/count", api.Feature(types.FEATURE_MONITORING, api.CountMetrics)).Methods(http.MethodPost)
 
-	xbanr := r.PathPrefix("/api/xban").Subrouter()
+	mr := apir.PathPrefix("/mail").Subrouter()
+	mr.Use(SecureHandler(api.PrivCheck(types.PRIV_INTERACT), api.FeatureCheck(types.FEATURE_MAIL)))
+	mr.HandleFunc("/folder/inbox", api.Secure(api.GetInbox)).Methods(http.MethodGet)
+	mr.HandleFunc("/folder/outbox", api.Secure(api.GetOutbox)).Methods(http.MethodGet)
+	mr.HandleFunc("/folder/drafts", api.Secure(api.GetDrafts)).Methods(http.MethodGet)
+	mr.HandleFunc("/contacts", api.Secure(api.GetContacts)).Methods(http.MethodGet)
+	mr.HandleFunc("/", api.Secure(api.SendMail)).Methods(http.MethodPost)
+	mr.HandleFunc("/{id}", api.Secure(api.DeleteMail)).Methods(http.MethodDelete)
+	mr.HandleFunc("/{id}/read", api.Secure(api.MarkRead)).Methods(http.MethodPost)
+	mr.HandleFunc("/{id}/unread", api.Secure(api.MarkUnread)).Methods(http.MethodPost)
+	mr.HandleFunc("/checkrecipient/{recipient}", api.Secure(api.CheckValidRecipient)).Methods(http.MethodGet)
+
+	xbanr := apir.PathPrefix("/xban").Subrouter()
 	xbanr.Use(SecureHandler(api.PrivCheck(types.PRIV_BAN), api.FeatureCheck(types.FEATURE_XBAN)))
 	xbanr.HandleFunc("/status", api.GetBanDBStatus).Methods(http.MethodGet)
 	xbanr.HandleFunc("/records/{playername}", api.GetBanRecord).Methods(http.MethodGet)
@@ -86,22 +90,24 @@ func Setup(a *app.App) error {
 	xbanr.HandleFunc("/unban", api.UnbanPlayer).Methods(http.MethodPost)
 	xbanr.HandleFunc("/cleanup", api.CleanupBanDB).Methods(http.MethodPost)
 
-	r.HandleFunc("/api/bridge/execute_chatcommand", api.Feature(types.FEATURE_SHELL, api.Secure(api.ExecuteChatcommand))).Methods(http.MethodPost)
-	r.HandleFunc("/api/bridge/lua", api.Feature(types.FEATURE_LUASHELL, api.SecurePriv(types.PRIV_SERVER, api.ExecuteLua))).Methods(http.MethodPost)
-	r.HandleFunc("/api/bridge", api.CheckApiKey(a.Bridge.HandlePost)).Methods(http.MethodPost)
-	r.HandleFunc("/api/bridge", api.CheckApiKey(a.Bridge.HandleGet)).Methods(http.MethodGet)
+	apir.HandleFunc("/bridge/execute_chatcommand", api.Feature(types.FEATURE_SHELL, api.Secure(api.ExecuteChatcommand))).Methods(http.MethodPost)
+	apir.HandleFunc("/bridge/lua", api.Feature(types.FEATURE_LUASHELL, api.SecurePriv(types.PRIV_SERVER, api.ExecuteLua))).Methods(http.MethodPost)
+	apir.HandleFunc("/bridge", api.CheckApiKey(a.Bridge.HandlePost)).Methods(http.MethodPost)
+	apir.HandleFunc("/bridge", api.CheckApiKey(a.Bridge.HandleGet)).Methods(http.MethodGet)
 
-	r.HandleFunc("/api/media/index.mth", api.Feature(types.FEATURE_MEDIASERVER, a.Mediaserver.ServeHTTPIndex)).Methods(http.MethodPost)
-	r.HandleFunc("/api/media/stats", api.Feature(types.FEATURE_MEDIASERVER, api.GetMediaStats)).Methods(http.MethodGet)
-	r.HandleFunc("/api/media/{hash}", a.Mediaserver.ServeHTTPFetch).Methods(http.MethodGet)
-	r.HandleFunc("/api/media/scan", api.Feature(types.FEATURE_MEDIASERVER, api.SecurePriv(types.PRIV_SERVER, api.ScanMedia))).Methods(http.MethodPost)
+	msr := apir.PathPrefix("/media").Subrouter()
+	msr.Use(SecureHandler(api.FeatureCheck(types.FEATURE_MEDIASERVER)))
+	msr.HandleFunc("/index.mth", a.Mediaserver.ServeHTTPIndex).Methods(http.MethodPost)
+	msr.HandleFunc("/stats", api.GetMediaStats).Methods(http.MethodGet)
+	msr.HandleFunc("/{hash}", a.Mediaserver.ServeHTTPFetch).Methods(http.MethodGet)
+	msr.HandleFunc("/scan", api.SecurePriv(types.PRIV_SERVER, api.ScanMedia)).Methods(http.MethodPost)
 
-	r.HandleFunc("/api/mods/scan", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.ScanWorldDir))).Methods(http.MethodPost)
-	r.HandleFunc("/api/mods", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.GetMods))).Methods(http.MethodGet)
-	r.HandleFunc("/api/mods/{id}/update/{version}", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.UpdateModVersion))).Methods(http.MethodPost)
-	r.HandleFunc("/api/mods", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.CreateMod))).Methods(http.MethodPost)
-	r.HandleFunc("/api/mods/{id}", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.DeleteMod))).Methods(http.MethodDelete)
-	r.HandleFunc("/api/mods/{id}/status", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.ModStatus))).Methods(http.MethodGet)
+	apir.HandleFunc("/mods/scan", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.ScanWorldDir))).Methods(http.MethodPost)
+	apir.HandleFunc("/mods", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.GetMods))).Methods(http.MethodGet)
+	apir.HandleFunc("/mods/{id}/update/{version}", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.UpdateModVersion))).Methods(http.MethodPost)
+	apir.HandleFunc("/mods", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.CreateMod))).Methods(http.MethodPost)
+	apir.HandleFunc("/mods/{id}", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.DeleteMod))).Methods(http.MethodDelete)
+	apir.HandleFunc("/mods/{id}/status", api.Feature(types.FEATURE_MODMANAGEMENT, api.SecurePriv(types.PRIV_SERVER, api.ModStatus))).Methods(http.MethodGet)
 
 	// OAuth
 	api.app.OAuthServer.SetAllowGetAccessRequest(true)
@@ -110,8 +116,8 @@ func Setup(a *app.App) error {
 	api.app.OAuthServer.UserAuthorizationHandler = api.OAuthAuthHandler
 	api.app.OAuthServer.SetUserAuthorizationHandler(api.OauthUserAuthorizationHandler)
 
-	r.HandleFunc("/authorize", api.OAuthAuthorizeHandler)
-	r.HandleFunc("/token", api.OAuthTokenHandler)
+	apir.HandleFunc("/authorize", api.OAuthAuthorizeHandler)
+	apir.HandleFunc("/token", api.OAuthTokenHandler)
 
 	// static files
 	if os.Getenv("WEBDEV") == "true" {
