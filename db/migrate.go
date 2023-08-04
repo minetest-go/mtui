de2f4872119ad81b5c711682bbd0fb63c2baf493
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-04 08:49:26 +0200

    proper iofs sql migration

--- db/migrate.go
+++ db/migrate.go
@@ -3,12 +3,10 @@ package db
 import (
 	"database/sql"
 	"embed"
-	"net/http"
 
 	"github.com/golang-migrate/migrate/v4"
 	"github.com/golang-migrate/migrate/v4/database/sqlite"
-	"github.com/golang-migrate/migrate/v4/source"
-	"github.com/golang-migrate/migrate/v4/source/httpfs"
+	"github.com/golang-migrate/migrate/v4/source/iofs"
 	"github.com/sirupsen/logrus"
 	_ "modernc.org/sqlite"
 )
@@ -16,30 +14,18 @@ import (
 //go:embed migrations/*.sql
 var migrations embed.FS
 
-func init() {
-	source.Register("embed", &driver{})
-}
-
-type driver struct {
-	httpfs.PartialDriver
-}
-
-func (d *driver) Open(rawURL string) (source.Driver, error) {
-	err := d.PartialDriver.Init(http.FS(migrations), "migrations")
-	if err != nil {
-		return nil, err
-	}
-
-	return d, nil
-}
-
 func Migrate(db *sql.DB) error {
 	driver, err := sqlite.WithInstance(db, &sqlite.Config{})
 	if err != nil {
 		return err
 	}
 
-	m, err := migrate.NewWithDatabaseInstance("embed://", "sqlite", driver)
+	d, err := iofs.New(migrations, "migrations")
+	if err != nil {
+		return err
+	}
+
+	m, err := migrate.NewWithInstance("iofs", d, "sqlite", driver)
 	if err != nil {
 		return err
 	}
