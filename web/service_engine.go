b1db2b774dc41255ac4e11b2e7e40f8b7ecb0ac9
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-09 21:38:00 +0200

    catch error

--- web/service_engine.go
+++ web/service_engine.go
@@ -146,7 +146,11 @@ func (a *Api) CreateEngine(w http.ResponseWriter, r *http.Request, claims *types
 		return
 	}
 	defer reader.Close()
-	io.Copy(os.Stdout, reader)
+	_, err = io.Copy(os.Stdout, reader)
+	if err != nil {
+		SendError(w, 500, fmt.Sprintf("io-copy error: %v", err))
+		return
+	}
 
 	world_dir := os.Getenv("DOCKER_WORLD_DIR")
 	world_dir_container := "/world"
