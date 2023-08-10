2facc49540898c9785b4b2c52a94b74f9da68bc7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-10 20:27:33 +0200

    remove links and embedded file for i18n lib

--- public/embed.go
+++ public/embed.go
@@ -8,7 +8,6 @@ import (
 //go:embed node_modules/bootstrap/dist/css/bootstrap.min.css
 //go:embed node_modules/vue/dist/vue.global.prod.js
 //go:embed node_modules/vue-router/dist/vue-router.global.prod.js
-//go:embed node_modules/vue-i18n/dist/vue-i18n.global.prod.js
 //go:embed node_modules/@fortawesome/fontawesome-free/css/all.min.css
 //go:embed node_modules/@fortawesome/fontawesome-free/webfonts/*
 //go:embed node_modules/@vuepic/vue-datepicker/dist/main.css
