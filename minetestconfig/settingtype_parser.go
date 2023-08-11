7940a605e5115f5a10392cf97b1f0ed028a4e01a
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2023-08-11 10:02:12 +0200

    add tests

--- minetestconfig/settingtype_parser.go
+++ minetestconfig/settingtype_parser.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
@@ -12,7 +13,8 @@ func ParseSettingTypes(data []byte) ([]*SettingType, error) {
 
 	list := make([]*SettingType, 0)
 
-	var previous_comment string
+	last_comment := ""
+	categories := []string{}
 
 	for sc.Scan() {
 		line := strings.TrimSpace(sc.Text())
@@ -23,19 +25,79 @@ func ParseSettingTypes(data []byte) ([]*SettingType, error) {
 		}
 
 		if strings.HasPrefix(line, "#") {
-			// remember prefix and continue
-			previous_comment = strings.TrimSpace(strings.TrimPrefix(line, "#"))
+			// append comment
+			last_comment = fmt.Sprintf("%s\n%s", last_comment, strings.TrimSpace(strings.TrimPrefix(line, "#")))
 			continue
 		}
 
-		// remove repeated spaces
-		for strings.Contains(line, "  ") {
-			line = strings.ReplaceAll(line, "  ", " ")
+		if strings.HasPrefix(line, "[") {
+			// category
+			category_depth := strings.Count(line, "*")
+			category := strings.NewReplacer("[", "", "]", "", "*", "").Replace(line)
+			if len(categories) > category_depth {
+				// strip outer categories
+				categories = categories[0:category_depth]
+			}
+			categories = append(categories, category)
+			continue
 		}
 
-		parts := strings.Split(line, " ")
+		// everything else is a settingtype entry
+		s := &SettingType{
+			LongDescription: last_comment,
+			Category:        categories,
+		}
+		// reset comment for next entry
+		last_comment = ""
 
-		fmt.Println(previous_comment, parts)
+		// disassemble setting line
+		parts := strings.Split(line, "(")
+		if len(parts) < 2 {
+			// "(" not found, skip
+			continue
+		}
+		s.Key = strings.TrimSpace(parts[0])
+
+		descparts := strings.Split(parts[1], ")")
+		if len(descparts) < 2 {
+			// ")" not found
+			continue
+		}
+		s.ShortDescription = strings.TrimSpace(descparts[0])
+
+		rest := strings.TrimSpace(descparts[1])
+		// remove double spaces
+		for strings.Contains(rest, "  ") {
+			rest = strings.ReplaceAll(rest, "  ", " ")
+		}
+
+		parts = strings.Split(rest, " ")
+		if len(parts) < 2 {
+			// not enough parts
+			continue
+		}
+		s.Type = strings.TrimSpace(parts[0])
+		s.Default = strings.TrimSpace(parts[1])
+
+		if len(parts) >= 3 {
+			// float 600.0 0.0
+			v, err := strconv.ParseFloat(parts[2], 64)
+			if err != nil {
+				return nil, fmt.Errorf("invalid 'min' setting in '%s': %v", s.Key, err)
+			}
+			s.Min = v
+		}
+
+		if len(parts) >= 4 {
+			// int 20 -1 32767
+			v, err := strconv.ParseFloat(parts[3], 64)
+			if err != nil {
+				return nil, fmt.Errorf("invalid 'max' setting in '%s': %v", s.Key, err)
+			}
+			s.Max = v
+		}
+
+		list = append(list, s)
 	}
 
 	return list, nil
--- minetestconfig/types.go
+++ minetestconfig/types.go
@@ -3,9 +3,12 @@ package minetestconfig
 type MinetestConfig map[string]string
 
 type SettingType struct {
-	LongDescription  string `json:"long_description"`
-	ShortDescription string `json:"short_description"`
-	Key              string `json:"key"`
-	Type             string `json:"type"`
-	Default          string `json:"default"`
+	Category         []string `json:"topic"` // stacked list of categories ["Advanced", "Developer Options", "Mod Security"]
+	LongDescription  string   `json:"long_description"`
+	ShortDescription string   `json:"short_description"`
+	Key              string   `json:"key"`  // my.key
+	Type             string   `json:"type"` // int, string, float, bool
+	Default          string   `json:"default"`
+	Min              float64  `json:"min"`
+	Max              float64  `json:"max"`
 }
