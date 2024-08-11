43b0e056cc472552b9141488acf0e540d4278623
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-08-11 19:07:57 +0200

    add mt engine 5.9.0 / mapserver 4.9.1

--- types/services.go
+++ types/services.go
@@ -4,10 +4,11 @@ var EngineServiceImages = map[string]string{
 	"5.6.0": "registry.gitlab.com/minetest/minetest/server:5.6.0",
 	"5.7.0": "registry.gitlab.com/minetest/minetest/server:5.7.0",
 	"5.8.0": "ghcr.io/minetest-hosting/minetest-docker:5.8.0",
+	"5.9.0": "ghcr.io/minetest/minetest:5.9.0",
 }
 
 // for auto install
-var EngineServiceLatest = "5.8.0"
+var EngineServiceLatest = "5.9.0"
 
 var MatterbridgeServiceImages = map[string]string{
 	"1.26.0": "42wim/matterbridge:1.26.0",
@@ -16,5 +17,6 @@ var MatterbridgeServiceImages = map[string]string{
 var MapserverServiceImages = map[string]string{
 	"4.7.0":  "minetestmapserver/mapserver:4.7.0",
 	"4.8.0":  "minetestmapserver/mapserver:4.8.0",
-	"latest": "minetestmapserver/mapserver:latest",
+	"4.9.1":  "ghcr.io/minetest-mapserver/mapserver:v4.9.1",
+	"latest": "ghcr.io/minetest-mapserver/mapserver:latest",
 }
