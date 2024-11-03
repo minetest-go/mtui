167122444a5477cf1aaf6e28fdd69b72a4f6bb4d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2024-11-03 16:53:22 +0100

    default to sqlite database if nothing compatible found (postgres/sqlite)

--- app/database.go
+++ app/database.go
@@ -5,6 +5,7 @@ import (
 	"mtui/db"
 	"mtui/mail"
 	"mtui/modmanager"
+	"path"
 
 	oautherrors "github.com/go-oauth2/oauth2/v4/errors"
 	"github.com/go-oauth2/oauth2/v4/generates"
@@ -13,6 +14,7 @@ import (
 	"github.com/go-oauth2/oauth2/v4/store"
 	"github.com/golang-jwt/jwt/v4"
 	"github.com/minetest-go/mtdb"
+	"github.com/minetest-go/mtdb/worldconfig"
 	"github.com/sirupsen/logrus"
 )
 
@@ -21,22 +23,52 @@ func (a *App) AttachDatabase() error {
 		"worlddir": a.WorldDir,
 	}).Info("Attaching database")
 
-	dbctx, err := mtdb.New(a.WorldDir)
+	wc, err := worldconfig.Parse(path.Join(a.WorldDir, "world.mt"))
 	if err != nil {
-		return err
+		return fmt.Errorf("error parsing world.mt: %v", err)
+	}
+
+	supported_backends := map[string]bool{
+		worldconfig.BACKEND_POSTGRES: true,
+		worldconfig.BACKEND_SQLITE3:  true,
+	}
+
+	if !supported_backends[wc[worldconfig.CONFIG_MAP_BACKEND]] {
+		fmt.Printf("Warning: unsupported map-backend: '%s' found, defaulting to sqlite!\n", wc[worldconfig.CONFIG_MAP_BACKEND])
+		wc[worldconfig.CONFIG_MAP_BACKEND] = worldconfig.BACKEND_SQLITE3
+	}
+
+	if !supported_backends[wc[worldconfig.CONFIG_AUTH_BACKEND]] {
+		fmt.Printf("Warning: unsupported auth-backend: '%s' found, defaulting to sqlite!\n", wc[worldconfig.CONFIG_AUTH_BACKEND])
+		wc[worldconfig.CONFIG_AUTH_BACKEND] = worldconfig.BACKEND_SQLITE3
+	}
+
+	if !supported_backends[wc[worldconfig.CONFIG_MOD_STORAGE_BACKEND]] {
+		fmt.Printf("Warning: unsupported mod_storage-backend: '%s' found, defaulting to sqlite!\n", wc[worldconfig.CONFIG_MOD_STORAGE_BACKEND])
+		wc[worldconfig.CONFIG_MOD_STORAGE_BACKEND] = worldconfig.BACKEND_SQLITE3
+	}
+
+	if !supported_backends[wc[worldconfig.CONFIG_PLAYER_BACKEND]] {
+		fmt.Printf("Warning: unsupported player-backend: '%s' found, defaulting to sqlite!\n", wc[worldconfig.CONFIG_PLAYER_BACKEND])
+		wc[worldconfig.CONFIG_PLAYER_BACKEND] = worldconfig.BACKEND_SQLITE3
+	}
+
+	dbctx, err := mtdb.NewWithConfig(a.WorldDir, wc)
+	if err != nil {
+		return fmt.Errorf("error creating database connection: %v", err)
 	}
 	a.DBContext = dbctx
 
 	db_, g, err := db.Init(a.WorldDir)
 	if err != nil {
-		return err
+		return fmt.Errorf("error initializing local database: %v", err)
 	}
 	a.DB = db_
 	a.G = g
 
 	err = db.Migrate(db_)
 	if err != nil {
-		return err
+		return fmt.Errorf("error migrating local database: %v", err)
 	}
 
 	a.Repos = db.NewRepositories(g)
