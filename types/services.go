4fc4fb3fb71505cc0a1358d26b52338282a79cb0
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-05-28 10:53:00 +0200

    update luanti and mapserver versions

--- types/services.go
+++ types/services.go
@@ -1,11 +1,10 @@
 package types
 
 var EngineServiceImages = map[string]string{
-	"5.7.0":  "registry.gitlab.com/minetest/minetest/server:5.7.0",
-	"5.8.0":  "ghcr.io/luanti-hosting/minetest-docker:5.8.0",
 	"5.9.0":  "ghcr.io/minetest/minetest:5.9.0",
 	"5.10.0": "ghcr.io/minetest/minetest:5.10.0",
 	"5.11.0": "ghcr.io/luanti-org/luanti:5.11.0",
+	"5.12.0": "ghcr.io/luanti-org/luanti:5.12.0",
 }
 
 // for auto install
@@ -16,8 +15,8 @@ var MatterbridgeServiceImages = map[string]string{
 }
 
 var MapserverServiceImages = map[string]string{
-	"4.7.0":  "minetestmapserver/mapserver:4.7.0",
 	"4.8.0":  "minetestmapserver/mapserver:4.8.0",
 	"4.9.1":  "ghcr.io/minetest-mapserver/mapserver:v4.9.1",
+	"4.9.3":  "ghcr.io/minetest-mapserver/mapserver:v4.9.3",
 	"latest": "ghcr.io/minetest-mapserver/mapserver:latest",
 }
