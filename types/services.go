98d991301c83e90c20a9bd15a4ef9a282a8c6f45
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2025-08-26 10:07:41 +0200

    update service versions

--- types/services.go
+++ types/services.go
@@ -5,10 +5,11 @@ var EngineServiceImages = map[string]string{
 	"5.10.0": "ghcr.io/minetest/minetest:5.10.0",
 	"5.11.0": "ghcr.io/luanti-org/luanti:5.11.0",
 	"5.12.0": "ghcr.io/luanti-org/luanti:5.12.0",
+	"5.13.0": "ghcr.io/luanti-org/luanti:5.13.0",
 }
 
 // for auto install
-var EngineServiceLatest = "5.12.0"
+var EngineServiceLatest = "5.13.0"
 
 var MatterbridgeServiceImages = map[string]string{
 	"1.26.0": "42wim/matterbridge:1.26.0",
@@ -18,5 +19,6 @@ var MapserverServiceImages = map[string]string{
 	"4.8.0":  "minetestmapserver/mapserver:4.8.0",
 	"4.9.1":  "ghcr.io/minetest-mapserver/mapserver:v4.9.1",
 	"4.9.3":  "ghcr.io/minetest-mapserver/mapserver:v4.9.3",
+	"4.9.4":  "ghcr.io/minetest-mapserver/mapserver:v4.9.4",
 	"latest": "ghcr.io/minetest-mapserver/mapserver:latest",
 }
